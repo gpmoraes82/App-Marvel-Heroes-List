a36bf8dceea495fedd0f7f5cf46b07f5bbdf09c5
Gabriel Palma Moraes <gpmoraes82@gmail.com>
2021-08-10 19:36:56 -0300

    Update Constants.java

--- AppMarvelHeroesList/app/src/main/java/com/example/appmarvelheroeslist/Extras/Constants.java
+++ AppMarvelHeroesList/app/src/main/java/com/example/appmarvelheroeslist/Extras/Constants.java
@@ -5,8 +5,8 @@ package com.example.appmarvelheroeslist.Extras;
 public class Constants {
 
     //---- API Keys ----
-    public static final String PUBLIC_KEY = "3043d757764057aee3fd1a7b23153899";
-    public static final String PRIVATE_KEY = "bcbb0f6a64d55948cc81d41b9124f2592f32de1f";
+    public static final String PUBLIC_KEY = "Change Me";
+    public static final String PRIVATE_KEY = "Change Me";
 
     //---- Url parts ----
     public static final String BASE_URL = "http://gateway.marvel.com";
